772464c7af23385d0fdf0e2add08895001d74918
Thodoris Kouleris <kurokokoro@windowslive.com>
2022-02-14 06:54:00 +0200

    feature: dynamic forecast

--- app.py
+++ app.py
@@ -1,20 +1,33 @@
-import json
 import os
-from pathlib import Path
 
 import requests
 from flask import Flask
 from dotenv import load_dotenv
+from flask_sqlalchemy import SQLAlchemy
+
 load_dotenv()
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 app.config['OWM_KEY'] = os.getenv('OWM_KEY')
+db = SQLAlchemy(app)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    url = "https://api.openweathermap.org/data/2.5/forecast?id=833&appid="+app.config['OWM_KEY']+"&units=metric"
+class City(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    city = db.Column(db.String(length=255), nullable=False, unique=False)
+    city_state = db.Column(db.String(length=255), nullable=False, unique=False)
+    country = db.Column(db.String(length=255), nullable=False, unique=False)
+    owm_id = db.Column(db.Integer(), nullable=True, unique=False)
+
+
+@app.route('/<city_id>')
+def hello_world(city_id):  # put application's code here
+    city = City.query.filter_by(id=city_id).first()
+    url = "https://api.openweathermap.org/data/2.5/forecast?id=" + str(city.owm_id) + "&appid=" + app.config[
+        'OWM_KEY'] + "&units=metric"
     response = requests.get(url)
     weather = response.json()['list']
     city = response.json()['city']['name']
